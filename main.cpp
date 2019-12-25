79d08c6b020fdfced58ede872c71e99c7b700e37
sjo98 <sjo98@daum.net>
2019-12-25 12:46:09 -0800

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,14 @@
 #include <errno.h>
 #include <string.h>
 #include <libnetfilter_queue/libnetfilter_queue.h>
+#include <iostream>
+#include <fstream>
 
 char hostname[100];
 int acc = 0;
 char method[6][8]={"GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS"};
 char string[7]="Host: ";
+
 void dump(unsigned char* buf, int size) {
 	int i;
 	acc =0;
@@ -104,7 +107,7 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
 		printf("blocked\n");
 		return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
 	}
-	else{
+	else{http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
 		printf("entering callback\n");
 		return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);	
 	}
@@ -113,56 +116,62 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
 
 int main(int argc, char **argv)
 {
-	int size = strlen(argv[1]);
-	memcpy(hostname, argv[1], size);
+	ifstream in(argv[1]);
 
-	struct nfq_handle *h;
-	struct nfq_q_handle *qh;
-	struct nfnl_handle *nh;
-	int fd;
-	int rv;
-	char buf[4096] __attribute__ ((aligned));
+	if(in.is_open()){
+		string tmp;
+		while(in){
+			getline(in, tmp);
+			int size = strlen(tmp);
+			memcpy(hostname, tmp, size);
+			struct nfq_handle *h;
+		struct nfq_q_handle *qh;
+		struct nfnl_handle *nh;
+		int fd;
+		int rv;
+		char buf[4096] __attribute__ ((aligned));
 
-	printf("opening library handle\n");
-	h = nfq_open();
-	if (!h) {
-		fprintf(stderr, "error during nfq_open()\n");
-		exit(1);
-	}
-
-	printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
-	if (nfq_unbind_pf(h, AF_INET) < 0) {
-		fprintf(stderr, "error during nfq_unbind_pf()\n");
-		exit(1);
-	}
-
-	printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
-	if (nfq_bind_pf(h, AF_INET) < 0) {
-		fprintf(stderr, "error during nfq_bind_pf()\n");
-		exit(1);
-	}
-
-	printf("binding this socket to queue '0'\n");
-	qh = nfq_create_queue(h,  0, &cb, NULL);
-	if (!qh) {
-		fprintf(stderr, "error during nfq_create_queue()\n");
-		exit(1);
-	}
-
-	printf("setting copy_packet mode\n");
-	if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
-		fprintf(stderr, "can't set packet_copy mode\n");
-		exit(1);
-	}
-
-	fd = nfq_fd(h);
-
-	for (;;) {
-		if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
-			printf("pkt received\n");
-			nfq_handle_packet(h, buf, rv);
-			continue;
+	
+		printf("opening library handle\n");
+		h = nfq_open();
+		if (!h) {
+			fprintf(stderr, "error during nfq_open()\n");
+			exit(1);
 		}
+
+		printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
+		if (nfq_unbind_pf(h, AF_INET) < 0) {
+			fprintf(stderr, "error during nfq_unbind_pf()\n");
+			exit(1);
+		}
+
+		printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+		if (nfq_bind_pf(h, AF_INET) < 0) {
+			fprintf(stderr, "error during nfq_bind_pf()\n");
+			exit(1);
+		}
+
+		printf("binding this socket to queue '0'\n");
+		qh = nfq_create_queue(h,  0, &cb, NULL);
+		if (!qh) {
+			fprintf(stderr, "error during nfq_create_queue()\n");
+			exit(1);
+		}
+
+		printf("setting copy_packet mode\n");
+		if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+			fprintf(stderr, "can't set packet_copy mode\n");
+			exit(1);
+		}
+
+		fd = nfq_fd(h);
+
+		for (;;) {
+			if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
+				printf("pkt received\n");
+				nfq_handle_packet(h, buf, rv);
+				continue;
+			}
 		/* if your application is too slow to digest the packets that
 		 * are sent from kernel-space, the socket buffer that we use
 		 * to enqueue packets may fill up returning ENOBUFS. Depending
@@ -170,26 +179,29 @@ int main(int argc, char **argv)
 		 * the doxygen documentation of this library on how to improve
 		 * this situation.
 		 */
-		if (rv < 0 && errno == ENOBUFS) {
-			printf("losing packets!\n");
-			continue;
+			if (rv < 0 && errno == ENOBUFS) {
+				printf("losing packets!\http://s3.amazonaws.com/alexa-static/top-1m.csv.zipn");
+				continue;
+			}
+			perror("recv failed");
+			break;
 		}
-		perror("recv failed");
-		break;
-	}
 
-	printf("unbinding from queue 0\n");
-	nfq_destroy_queue(qh);
+		printf("unbinding from queue 0\n");
+		nfq_destroy_queue(qh);
 
 #ifdef INSANE
 	/* normally, applications SHOULD NOT issue this command, since
 	 * it detaches other programs/sockets from AF_INET, too ! */
-	printf("unbinding from AF_INET\n");
-	nfq_unbind_pf(h, AF_INET);
+		printf("unbinding from AF_INET\n");
+		nfq_unbind_pf(h, AF_INET);
 #endif
 
-	printf("closing library handle\n");
-	nfq_close(h);
+		printf("closing library handle\n");
+		nfq_close(h);
 
-	exit(0);
+		exit(0);
+
+		}
+	}
 }
